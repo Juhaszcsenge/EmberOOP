7dcaea3ec9c43b83f1e04559c29235974d66e473
Eszaki Daniel <info@petrik.hu>
2022-09-22 17:45:24 +0200

    Folytatás és berolvastuk a txt fájlt

--- src/hu/petrik/emberekoop/Emberek.java
+++ src/hu/petrik/emberekoop/Emberek.java
@@ -1,5 +1,9 @@
 package hu.petrik.emberekoop;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,5 +18,26 @@ public class Emberek {
         for(Ember e: emberTomb{
         this.emberek.add(e);
          */
+    }
+
+    public Emberek(String fajlNev) throws IOException {
+        FileReader fr = new FileReader(fajlNev);
+        BufferedReader br = new BufferedReader(fr);
+
+        fr.close();
+        br.close();
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder(String.format("%30s %10s (%3s év) %20s",
+                "Név", "Szül.dátum", "Kor", "Szül.hely"));
+        for(int i = 0; i < this.emberek.size(); i++){
+            s.append(System.lineSeparator()).append(this.emberek.get(i));
+        }
+
+
+        return s.toString();
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,13 +1,35 @@
 import hu.petrik.emberekoop.Ember;
+import hu.petrik.emberekoop.Emberek;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class main {
     public static void main(String[] args) {
          Ember e1 = new Ember("Gipsz Jakab", "2002-2-9","Budapest");
          Ember e2 = new Ember("Teszt Elek", "2004-10-27","Visegrád");
          Ember e3 = new Ember("Máté Péter", "1978-5-12","Kukutyin");
+         Ember e4 = new Ember("József Péter", "1978-5-12","Falu");
+         Ember e5 = new Ember("Zsiga Péter", "1978-5-12","Érdekes");
+         Ember[] emberTomb = new Ember[]{e1, e2, e3, e4, e5};
+         Emberek ek = new Emberek(emberTomb);
+         String fajlNev = "sajt.txt";
+         try{
+             Emberek ek2 = new Emberek(fajlNev);
+         }
+         catch (FileNotFoundException e) {
+             System.out.printf("A %s nem található\n", fajlNev);
+         }catch (IOException e) {
+             System.out.println("Ismeretlen hiba történt");
+         }
+         }
+
+       /* System.out.println(ek);
 
         System.out.println(e1);
         System.out.println(e2);
         System.out.println(e3);
+        */
+
     }
-}
+
